0bf0413c3bcd7876b11cc4b2aa351a4ba6b7bced
Carlos Loyola <caloyola84@gmail.com>
2020-04-21 10:52:22 -0300

    ajustes dos scripts

--- 01-pacotes-basicos.sh
+++ 01-pacotes-basicos.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sudo apt install -y build-essential \
-                 apt-listchanges \
-                 apt-listbugs \
+                    apt-listchanges \
+                    apt-listbugs \
